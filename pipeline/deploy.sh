c94e5576c6e38d1fdc96a532b9c8126e2476e443
Joao <joao@wobreeze.com>
2024-11-07 19:15:15 +0700

    update

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -6,19 +6,22 @@ set -euo pipefail
 export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile ${AWS_CREDENTIALS_PROFILE})
 export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile ${AWS_CREDENTIALS_PROFILE})
 
+cp flask/.env flask/.env.bak
+aws secretsmanager get-secret-value --region $REGION --secret-id $DEPLOYMENT_TARGET-$APP-env --query SecretString --output text > flask/.env
 
-cp .env .env.bak
-aws secretsmanager get-secret-value --region $REGION --secret-id $DEPLOYMENT_TARGET-$APP-env --query SecretString --output text > .env
+# Setup buildx
+docker buildx create --name mybuilder --use
+docker buildx install
 
-# Build & Push docker image
-docker build -t $ECR/$DOCKER_REPO:$IMAGE_TAG .
-
-# Authenticate to AWS ECR and Push docker image
+# Authenticate to AWS ECR
 aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR
-docker push $ECR/$DOCKER_REPO:$IMAGE_TAG
+
+# Build & Push docker image with increased verbosity
+
+docker buildx build --provenance=false --platform linux/amd64,linux/arm64 -t $ECR/$DOCKER_REPO:$IMAGE_TAG --push --progress=plain .
 
 # restore .env file
-mv .env.bak .env
+mv flask/.env.bak flask/.env
 
 # # Deploy to docker swarm
 ssh -o StrictHostKeyChecking=no -i /root/.ssh/key.pem $HOST "
